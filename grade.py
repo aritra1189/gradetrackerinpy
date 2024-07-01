28a65d9ff50ec6815d0ad91cab9b87cf815a65a4
aritra sharma <98529719+aritra1189@users.noreply.github.com>
2024-07-01 21:54:10 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,232 @@
+print("Enter Marks Obtained in 5 Subjects: ")
+
+num1 = float(input("Enter sub1 number: "))
+if num1 >= 91 and num1 <= 100:
+
+    print("Your subject Grade is A1")
+
+elif num1 >= 81 and num1 < 91:
+
+    print("Your subject Grade is A2")
+
+elif num1 >= 71 and num1 < 81:
+
+    print("Your subject Grade is B1")
+
+elif num1 >= 61 and num1 < 71:
+
+    print("Your subject Grade is B2")
+
+elif num1 >= 51 and num1 < 61:
+
+    print("Your subject Grade is C1")
+
+elif num1 >= 41 and num1 < 51:
+
+    print("Your subject Grade is C2")
+
+elif num1 >= 33 and num1 < 41:
+
+    print("Your subject Grade is D")
+
+elif num1 >= 21 and num1 < 33:
+
+    print("Your subject Grade is E1")
+
+elif num1 >= 0 and num1 < 21:
+
+    print("Your subject Grade is E2")
+num2 = float(input("Enter sub2 number: "))
+if num2 >= 91 and num2 <= 100:
+
+    print("Your subject Grade is A1")
+
+elif num2 >= 81 and num2 < 91:
+
+    print("Your subject Grade is A2")
+
+elif num2 >= 71 and num2 < 81:
+
+    print("Your subject Grade is B1")
+
+elif num2 >= 61 and num2 < 71:
+
+    print("Your subject Grade is B2")
+
+elif num2 >= 51 and num2 < 61:
+
+    print("Your subject Grade is C1")
+
+elif num2 >= 41 and num2 < 51:
+
+    print("Your subject Grade is C2")
+
+elif num2 >= 33 and num2 < 41:
+
+    print("Your subject Grade is D")
+
+elif num2 >= 21 and num2 < 33:
+
+    print("Your subject Grade is E1")
+
+elif num2 >= 0 and num2 < 21:
+
+    print("Your subject Grade is E2")
+num3 = float(input("Enter sub3 number: "))
+if num3 >= 91 and num3 <= 100:
+
+    print("Your subject Grade is A1")
+
+elif num3 >= 81 and num3 < 91:
+
+    print("Your subject Grade is A2")
+
+elif num3 >= 71 and num3 < 81:
+
+    print("Your subject Grade is B1")
+
+elif num3 >= 61 and num3 < 71:
+
+    print("Your subject Grade is B2")
+
+elif num3 >= 51 and num3 < 61:
+
+    print("Your subject Grade is C1")
+
+elif num3 >= 41 and num3 < 51:
+
+    print("Your subject Grade is C2")
+
+elif num3 >= 33 and num3 < 41:
+
+    print("Your subject Grade is D")
+
+elif num3 >= 21 and num3 < 33:
+
+    print("Your subject Grade is E1")
+
+elif num3 >= 0 and num3 < 21:
+
+    print("Your subject Grade is E2")
+
+num4 = float(input("Enter sub4 number: "))
+if num4 >= 91 and num4 <= 100:
+
+    print("Your subject Grade is A1")
+
+elif num4 >= 81 and num4 < 91:
+
+    print("Your subject Grade is A2")
+
+elif num4 >= 71 and num4 < 81:
+
+    print("Your subject Grade is B1")
+
+elif num4 >= 61 and num4 < 71:
+
+    print("Your subject Grade is B2")
+
+elif num4 >= 51 and num4 < 61:
+
+    print("Your subject Grade is C1")
+
+elif num4 >= 41 and num4 < 51:
+
+    print("Your subject Grade is C2")
+
+elif num4 >= 33 and num4 < 41:
+
+    print("Your subject Grade is D")
+
+elif num4 >= 21 and num4 < 33:
+
+    print("Your subject Grade is E1")
+
+elif num4 >= 0 and num4 < 21:
+
+    print("Your subject Grade is E2")
+num5 = float(input("Enter sub5 number: "))
+if num4 >= 91 and num4 <= 100:
+
+    print("Your subject Grade is A1")
+
+elif num4 >= 81 and num4 < 91:
+
+    print("Your subject Grade is A2")
+
+elif num4 >= 71 and num4 < 81:
+
+    print("Your subject Grade is B1")
+
+elif num4 >= 61 and num4 < 71:
+
+    print("Your subject Grade is B2")
+
+elif num4 >= 51 and num4 < 61:
+
+    print("Your subject Grade is C1")
+
+elif num4 >= 41 and num4 < 51:
+
+    print("Your subject Grade is C2")
+
+elif num4 >= 33 and num4 < 41:
+
+    print("Your subject Grade is D")
+
+elif num4 >= 21 and num4 < 33:
+
+    print("Your subject Grade is E1")
+
+elif num4 >= 0 and num4 < 21:
+
+    print("Your subject Grade is E2")
+           
+ 
+
+tot = num1+num2+num3+num4+num5
+
+avg = tot / 5
+if avg >= 91 and avg <= 100:
+
+    print("Your avarage Grade is A1")
+
+elif avg >= 81 and avg < 91:
+
+    print("Your avarage Grade is A2")
+
+elif avg >= 71 and avg < 81:
+
+    print("Your avarage Grade is B1")
+
+elif avg >= 61 and avg < 71:
+
+    print("Your avarage Grade is B2")
+
+elif avg >= 51 and avg < 61:
+
+    print("Your avarage Grade is C1")
+
+elif avg >= 41 and avg < 51:
+
+    print("Your avarage Grade is C2")
+
+elif avg >= 33 and avg < 41:
+
+    print("Your avarage Grade is D")
+
+elif avg >= 21 and avg < 33:
+
+    print("Your avarage Grade is E1")
+
+elif avg >= 0 and avg < 21:
+
+    print("Your avarage Grade is E2")
+
+else:
+
+    print("Invalid Input!")
+
+cgpa=avg/10
+print("your cgpa is:",cgpa)
+
